2f99bf2b81f824b9fbcdf38705ac334d138f985c
abdulmujeeb4ahmed <abdulmujeeb4ahmed@gmail.com>
2025-02-05 16:51:32 -0500

    Initial project setup and structure

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherHomePage(),
+    );
+  }
+}
+
+class WeatherHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Weather Info App")),
+      body: Center(child: Text("Welcome to the Weather Info App")),
+    );
+  }
+}
